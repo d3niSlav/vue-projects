6ca31c68f52555e2699e6b2cf95414418d67f216
Denislav Enchev <deni.b.enchev@gmail.com>
2019-02-27 19:10:56 +0200

    Added attack, special attack and heal actions.

--- vue-simple-fight-game/app.js
+++ vue-simple-fight-game/app.js
@@ -10,6 +10,54 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttack();
+    },
+    useSpecialAttack: function () {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttack();
+    },
+    heal: function () {
+      this.playerHealth = this.playerHealth <= 90 ? this.playerHealth += 10 : 100;
+      this.monsterAttack();
+    },
+    giveUp: function () {
+
+    },
+    monsterAttack: function () {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+    calculateDamage: function (minimumDamage, maximumDamage) {
+      return Math.max(Math.floor(Math.random() * maximumDamage) + 1, minimumDamage);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        this.promptMessage('You won!');
+        return true;
+      } else if (this.playerHealth <= 0) {
+        this.promptMessage('You lost!');
+        return true;
+      }
+
+      return false;
+    },
+    promptMessage: function (message) {
+      if (confirm(message + ' Do you want to start a new game?')) {
+        this.startGame();
+      } else {
+        this.gameIsRunning = false;
+      }
     }
   }
 });
